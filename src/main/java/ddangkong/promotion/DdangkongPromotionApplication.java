200abef9c6ac9d6268532940f60cea8c42e77465
leegwichan <cnddkscndgus@naver.com>
2024-12-16 12:09:09 +0900

    init: Application 세팅

---
+++ src/main/java/ddangkong/promotion/DdangkongPromotionApplication.java
@@ -0,0 +1,13 @@
+package ddangkong.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangkongPromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangkongPromotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddangkong/promotion/DdangkongPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package ddangkong.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdangkongPromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
